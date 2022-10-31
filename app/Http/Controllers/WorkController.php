faaa86f52b84cdde8d59a0a2f3832951a88dcda5
Brandon Giampa <30915668+brandongiampa@users.noreply.github.com>
2022-10-31 08:57:28 -0700

    create custom validations and sanitizations for work data

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Response;
 use App\Models\Work;
 
 define('NUMBER_OF_WORKS_PER_PAGE', 20);
+define('MINIMUM_DESCRIPTION_WORD_COUNT', 50);
+define('MSG_UNKNOWN_ERROR', 'There was an unknown error when processing your request.');
 
 class WorkController extends Controller
 {
@@ -25,7 +27,9 @@ class WorkController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id) {
-        return Work::find($id);
+        $work = Work::find($id);
+        if ($work) return $work;
+        else return response(MSG_UNKNOWN_ERROR, 500); 
     }
 
     /**
@@ -55,21 +59,11 @@ class WorkController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request) {
-        $request->validate([
-            'name' => 'Required|Unique:works,name',
-            'subheader' => 'Required',
-            'description' => 'Required',
-            'hashtags' => 'Required',
-            'slug' => 'Required',
-            'img_url' => 'Required|URL',
-            'carousel_img_urls' => 'Required',
-            'site_url' => 'Required|URL',
-            'github_url' => 'Required|URL|Regex:/https:\/\/(www\.)?github\.com\/.+/',
-            'features' => 'Required',
-            'technologies' => 'Required'
-        ]);
+        $request = $this->sanitize_split_strings($request);
+        $msgs = $this->get_error_messages_array_for_post_request($request);
 
-        return Work::create($request->all());
+        if (sizeof($msgs) < 1) return Work::create($request->all());
+        else return response($msgs, 400);
     }
 
     /**
@@ -79,24 +73,51 @@ class WorkController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id) {
-        $nameValidator = '';
+        $arr = array();
         $work = Work::find($id);
+
+        $request = $this->sanitize_split_strings($request);
 
         if (isset($request['name'])) {
             if ($request['name'] !== $work['name']) {
-                $nameValidator = 'Unique:works,name';
+                if (Work::where('name', $request['name'])->count() > 0) {
+                    array_push($arr, "This name already exists in our database.");
+                }
+            }
+        }
+        if (isset($request['img_url'])) {
+            if (!filter_var($request['img_url'], FILTER_VALIDATE_URL)) {
+                array_push($arr, "Please use a valid URL for the image.");
+            }
+        }
+        if (isset($request['carousel_img_urls'])) {
+            $explode = explode(',', $request['carousel_img_urls']);
+            foreach ($explode as $carousel_img_url) {
+                if (!filter_var($carousel_img_url, FILTER_VALIDATE_URL)) {
+                    array_push($arr, "Please use a valid URL for all carousel images.");
+                    break;
+                }
+            }
+        }
+        if (isset($request['site_url'])) {
+            if (!filter_var($request['site_url'], FILTER_VALIDATE_URL)) {
+                array_push($arr, "Please use a valid URL for the website.");
+            }
+        }
+        if (isset($request['github_url'])) {
+            if (!filter_var($request['github_url'], FILTER_VALIDATE_URL)) {
+                array_push($arr, "Please use a valid Github URL..");
+            }
+            if (!preg_match('/https:\/\/(www\.)?github\.com\/.+/', $request['github_url'])) {
+                array_push($arr, "The URL you provided for Github does not point to the Github website.");
             }
         }
 
-        $request->validate([
-            'name' => $nameValidator,
-            'img_url' => 'URL',
-            'site_url' => 'URL',
-            'github_url' => 'URL|Regex:/https:\/\/(www\.)?github\.com\/.+/'
-        ]);
-
-        $work->update($request->all());
-        return $work;
+        if (sizeof($arr) < 1) {
+            if ($work->update($request->all())) return $work;
+            else return response(['messages'=>'There was an unknown error trying to process your request.'], 500);
+        }
+        else return response(['messages'=>$arr], 500);
     }
 
     /**
@@ -110,5 +131,108 @@ class WorkController extends Controller
         $str = $work > 0 ? "Work at id #".$id." has been deleted." : "Error deleting work at id #".$id.".";
         $responseCode = $work > 0 ? 200 : 400;
         return response(['messages'=>[$str]], $responseCode);
+    }
+
+    /**
+     * Validate input object properties before creating a resource.
+     *
+     * @param  arr  $request
+     * @return arr  $request
+     */
+    private function get_error_messages_array_for_request($request) {
+        $arr = array();
+
+        //Validations
+        if (!isset($request['name'])) array_push($arr, "No name specified.");
+        if (isset($request['name'])) {
+            if (Work::where('name', $request['name'])->count() > 0) {
+                array_push($arr, "This name already exists in our database.");
+            }
+        }
+        if (!isset($request['subheader'])) array_push($arr, "No subheader specified.");
+        if (!isset($request['description'])) array_push($arr, "No description specified.");
+        if (isset($request['description'])) {
+            if (sizeof(explode(" ", $request['description'])) < MINIMUM_DESCRIPTION_WORD_COUNT) {
+                array_push($arr, "Please make your description at least 50 words long.");
+            }
+        }
+        if (!isset($request['hashtags'])) array_push($arr, "No hashtags specified.");
+        if (!isset($request['slug'])) array_push($arr, "No slug specified.");
+        if (!isset($request['img_url'])) array_push($arr, "No image URL specified.");
+        if (isset($request['img_url'])) {
+            if (!filter_var($request['img_url'], FILTER_VALIDATE_URL)) {
+                array_push($arr, "Please use a valid URL for the image.");
+            }
+        }
+        if (!isset($request['carousel_img_urls'])) array_push($arr, "Plesae specify at least one image URL for the carousel.");
+        if (isset($request['carousel_img_urls'])) {
+            $explode = explode(',', $request['carousel_img_urls']);
+            foreach ($explode as $carousel_img_url) {
+                if (!filter_var($carousel_img_url, FILTER_VALIDATE_URL)) {
+                    array_push($arr, "Please use a valid URL for all carousel images.");
+                    break;
+                }
+            }
+        }
+        if (!isset($request['site_url'])) array_push($arr, "No site URL specified.");
+        if (isset($request['site_url'])) {
+            if (!filter_var($request['site_url'], FILTER_VALIDATE_URL)) {
+                array_push($arr, "Please use a valid URL for the webite.");
+            }
+        }
+        if (!isset($request['github_url'])) array_push($arr, "No Github URL specified.");
+        if (isset($request['github_url'])) {
+            if (!filter_var($request['github_url'], FILTER_VALIDATE_URL)) {
+                array_push($arr, "Please use a valid Github URL..");
+            }
+            if (!preg_match('/https:\/\/(www\.)?github\.com\/.+/', $request['github_url'])) {
+                array_push($arr, "The URL you provided for Github does not point to the Github website.");
+            }
+        }
+        if (!isset($request['technologies'])) array_push($arr, "No technologies specified.");
+        if (!isset($request['features'])) array_push($arr, "No features specified.");
+        return $arr;
+    }
+
+    /**
+     * Trim strings in request intended to be exploded by comma separators.
+     *
+     * @param  arr  $request
+     * @return arr  $request
+     */
+    private function sanitize_split_strings($request) {
+        if (isset($request['hashtags'])) {
+            $explode = explode(',', $request['hashtags']);
+            
+            for ($i=0; $i<sizeof($explode); $i++) {
+                $explode[$i] = trim($explode[$i]);
+            }
+            $request['hashtags'] = implode(',', $explode);
+        }
+        if (isset($request['carousel_img_urls'])) {
+            $explode = explode(',', $request['carousel_img_urls']);
+            
+            for ($i=0; $i<sizeof($explode); $i++) {
+                $explode[$i] = trim($explode[$i]);
+            }
+            $request['carousel_img_urls'] = implode(',', $explode);
+        }
+        if (isset($request['technologies'])) {
+            $explode = explode(',', $request['technologies']);
+            
+            for ($i=0; $i<sizeof($explode); $i++) {
+                $explode[$i] = trim($explode[$i]);
+            }
+            $request['technologies'] = implode(',', $explode);
+        }
+        if (isset($request['features'])) {
+            $explode = explode(',', $request['features']);
+            
+            for ($i=0; $i<sizeof($explode); $i++) {
+                $explode[$i] = trim($explode[$i]);
+            }
+            $request['features'] = implode(',', $explode);
+        }
+        return $request;
     }
 }
