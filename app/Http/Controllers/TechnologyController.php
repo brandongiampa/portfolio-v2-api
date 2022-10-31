229901f888a2efcf09da49c59d2bdb387ed12f1c
Brandon Giampa <30915668+brandongiampa@users.noreply.github.com>
2022-10-30 19:58:12 -0700

    create custom validations for technology post/put

--- app/Http/Controllers/TechnologyController.php
+++ app/Http/Controllers/TechnologyController.php
@@ -23,15 +23,10 @@ class TechnologyController extends Controller
      * @param  str  $name
      * @return \Illuminate\Http\Response
      */
-    public function show_by_name($name)
-    {
-        $find = Technology::where('name', $name)->get();
-        return sizeof($find) !== 0 ? $find : response('No technology exists at this location.', 404);
-    }
 
     public function show($id) {
         $technology = Technology::find($id);
-        return sizeof($technology) > 0 ? $technology : response('No technology exists at this id.', 404);
+        return $technology ? $technology : response('No technology exists at this id.', 404);
     }
 
     /**
@@ -40,10 +35,25 @@ class TechnologyController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request) {
-        $request->validate(['name' => 'required|Unique:technologies,name','img_url' => 'required|URL']);
+        //$request->validate(['name' => 'required|Unique:technologies,name','img_url' => 'required|URL']);
+        $msgs = array();
+        if (!isset($request['name'])) array_push($msgs, "Please specify a technology name.");
+        if (isset($request['name'])) {
+            if (Technology::where('name', $request['name'])->count() > 0) {
+                array_push($msgs, "That name already exists in our database.");
+            }
+        }
+        if (!isset($request['img_url'])) array_push($msgs, "Please specify a URL for the corresponding img file.");
+        if (isset($request['img_url'])) {
+            if (!filter_var($request['img_url'], FILTER_VALIDATE_URL)) {
+                array_push($msgs, "Please specify a valid URL for the img file.");
+            }
+        }
+        if (sizeof($msgs) > 0) return response(['messages'=>$msgs], 400);
+
         $created = Technology::create($request->all());
         if ($created) return $created;
-        else return response('There was an error creating the requested technology. Please check that you are providing a valid URL.', 400);
+        else return response(['messages'=>'There was an unknown error creating the requested technology.'], 400);
     }
 
     /**
@@ -54,8 +64,28 @@ class TechnologyController extends Controller
      */
     public function update(Request $request, $id) {
         $technology = Technology::find($id);
-        $technology->update($request->all());
-        return $technology;
+        if (!$technology) return response(['messages'=>'There is no technology at that location.'], 404);
+
+        $msgs = array();
+
+        if (isset($request['name'])) {
+            $matching_technologies = Technology::where('name', $request['name'])->get();
+
+            if (sizeof($matching_technologies) > 1 || (sizeof($matching_technologies) === 1 && $matching_technologies[0]['id'] != $id)) {
+                array_push($msgs, "That name already exists in our database.");
+            }
+        }
+
+        if (isset($request['img_url'])) {
+            if (!filter_var($request['img_url'], FILTER_VALIDATE_URL)) {
+                array_push($msgs, "Please specify a valid URL for the img file.");
+            }
+        }
+        if (sizeof($msgs) < 1) {
+            if ($technology->update($request->all()) > 0) return $technology;
+            else return response(['messages'=>"There was an unknown error processing your request."], 400);
+        }
+        else return response(['messages'=>$msgs], 400);
     }
 
     /**
